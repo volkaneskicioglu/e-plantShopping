fb5fa36cc9f0acb85fa5f7ceedaf62cc60b721fb
Volkan Eskicioglu <volkaneskicioglu@gmail.com>
2026-05-21 09:49:19 -0400

    cart item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity <= 1){
+        dispatch(removeItem({ name: item.name }));
+   } else {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+   }
+
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
